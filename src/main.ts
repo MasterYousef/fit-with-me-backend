b3e4e3582b3e593e63cd66a1911b0a024f6737d3
MasterYousef <140777033+MasterYousef@users.noreply.github.com>
2025-05-09 14:35:02 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,24 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import { AppModule } from './app.module';
-import { CustomExceptionFilter } from './filters/CustomExceptionFilter';
-import { ValidationPipe } from '@nestjs/common';
-import { configDotenv } from 'dotenv';
-import fastifyMultipart from '@fastify/multipart';
-import cors from '@fastify/cors';
+} from "@nestjs/platform-fastify";
+import { AppModule } from "./app.module";
+import { CustomExceptionFilter } from "./filters/CustomExceptionFilter";
+import { ValidationPipe } from "@nestjs/common";
+import { configDotenv } from "dotenv";
+import fastifyMultipart from "@fastify/multipart";
+import cors from "@fastify/cors";
 
-configDotenv({ path: './src/config/config.env' });
+configDotenv({ path: "./src/config/config.env" });
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter(),
+    new FastifyAdapter()
   );
   await app.register(cors, {
-    origin: [
-      process.env.FRONTEND_URL,
-      'https://egypt-tours-backend.onrender.com',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   });
   await app.register(fastifyMultipart, {
     limits: {
@@ -32,11 +29,11 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-    }),
+    })
   );
-  app.setGlobalPrefix('api/v1');
-  await app.listen(process.env.PORT || 8000, '0.0.0.0', () => {
-    console.log('listening on port ' + process.env.PORT);
+  app.setGlobalPrefix("api/v1");
+  await app.listen(process.env.PORT || 8000, "0.0.0.0", () => {
+    console.log("listening on port " + process.env.PORT);
   });
 }
 bootstrap();
